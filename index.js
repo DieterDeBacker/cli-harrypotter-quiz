9a12da6c2f70a96f0d0f080fe2958dc04bf29109
Dieter De Backer <dieter.debacker.y0956@student.hogent.be>
2022-03-15 23:12:23 +0100

    🎉 Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+async function welcome() {
+  console.log(chalk.bgGreen("Hello world!"));
+}
+
+await welcome();
